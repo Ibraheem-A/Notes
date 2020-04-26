f76183e1b8183ff9152f209d8ceb994db6e0a06e
Ibraheem-A <43247583+Ibraheem-A@users.noreply.github.com>
2020-04-26 21:53:54 +0200

    added listView to layout

--- app/src/main/java/com/example/notes/NotesListActivity.java
+++ app/src/main/java/com/example/notes/NotesListActivity.java
@@ -2,11 +2,14 @@ package com.example.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class NotesListActivity extends AppCompatActivity {
@@ -19,6 +22,13 @@ public class NotesListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_notes_list);
 
         // List view & Adapter
-    }
+        listView = findViewById(R.id.notesListView);
 
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Yankee");
+        arrayList.add("Upper");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_selectable_list_item, arrayList);
+        listView.setAdapter(arrayAdapter);
+    }
 }
