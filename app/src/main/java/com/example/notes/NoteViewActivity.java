105067a872d5ed8845a5970a727448ab77e4ee44
Ibraheem-A <43247583+Ibraheem-A@users.noreply.github.com>
2020-04-27 14:55:34 +0200

    added feature to display clicked notes

--- app/src/main/java/com/example/notes/NoteViewActivity.java
+++ app/src/main/java/com/example/notes/NoteViewActivity.java
@@ -2,13 +2,25 @@ package com.example.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class NoteViewActivity extends AppCompatActivity {
+
+    TextView noteTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_view);
+
+        noteTextView = findViewById(R.id.noteTextView);
+
+        Intent intentFromNoteList = getIntent();
+        String currentNote = intentFromNoteList.getStringExtra("title");
+
+        noteTextView.setText(currentNote);
     }
 }
