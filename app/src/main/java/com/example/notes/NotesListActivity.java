01905808cbbc2dc71bbb40e9d64d20ee295aceea
Ibraheem-A <43247583+Ibraheem-A@users.noreply.github.com>
2020-04-27 14:00:56 +0200

    added onItemClickListener to launch NoteViewActivity

--- app/src/main/java/com/example/notes/NotesListActivity.java
+++ app/src/main/java/com/example/notes/NotesListActivity.java
@@ -6,6 +6,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -16,12 +18,6 @@ import java.util.List;
 public class NotesListActivity extends AppCompatActivity {
 
     ListView listView;
-    
-
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +27,20 @@ public class NotesListActivity extends AppCompatActivity {
         // List view & Adapter
         listView = findViewById(R.id.notesListView);
 
-        ArrayList<String> arrayList = new ArrayList<>();
+        final ArrayList<String> arrayList = new ArrayList<>();
         arrayList.add("Yankee");
         arrayList.add("Upper");
 
         ArrayAdapter arrayAdapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_selectable_list_item, arrayList);
         listView.setAdapter(arrayAdapter);
+
+        final Intent launchNoteView = new Intent(getApplicationContext(), NoteViewActivity.class);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                launchNoteView.putExtra("title", arrayList.get(position));
+                startActivity(launchNoteView);
+            }
+        });
     }
 }
